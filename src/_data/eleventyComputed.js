3716a5b695179e00c667cc7b9edb56845978034e
Gaelryndel <gaelryndel@pm.me>
2022-07-03 14:26:08 +0300

    11ty

--- src/_data/eleventyComputed.js
+++ src/_data/eleventyComputed.js
@@ -1,10 +1,12 @@
+const webmentions = require("../../lib/utils/getwm.js");
+
 module.exports = {
   pageTitle: (data) => `${data.title} · ${data.site.title}`,
   pageImage: (data) => (data.photo ? data.photo[0] || data.photo : false),
   canonicalUrl: (data) => (data.canonical && data.canonical.url ? data.canonical.url : data.site.url + data.page.url),
   photos: (data) => (data.photo ? (Array.isArray(data.photo) ? data.photo : [data.photo]) : false),
   webmentions: (data) => {
-    const url = data.app.url + data.page.url;
+    const url = data.site.url + data.page.url;
     return webmentions(data.webmentions.children, url);
   },
 };
